cea448c1cd0b0a2d38c2e9f1fa58124968c41df3
Leonardo Rocchi <leonardo.rocchi@yahoo.it>
2024-05-15 13:04:49 +0200

    adding dotdict

---
+++ src/utils.py
@@ -0,0 +1,1 @@
+"""Copyright (c) leonardo-rocchi:z4ir3"""class DotDict(dict):    """    dot.notation access to dictionary attributes    source: https://stackoverflow.com/questions/2352181/how-to-use-a-dot-to-access-members-of-dictionary    """    __getattr__ = dict.get    __setattr__ = dict.__setitem__    __delattr__ = dict.__delitem__
