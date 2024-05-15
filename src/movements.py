987fd426171e44699dc7426b14c3fc4a7d96cd78
Leonardo Rocchi <leonardo.rocchi@yahoo.it>
2024-05-15 13:30:26 +0200

    starting stretching

--- src/movements.py
+++ src/movements.py
@@ -64,14 +64,31 @@ def stretch(Obj: ClassVar, sfactor: float) -> ClassVar:
     if sfactor < 0:
         raise ValueError("The stretching scaling factor must be > 0")
 
-
     # New first vertex and corresponding center-to-vertex vector
     v1x = Obj.center.x + sfactor * Obj.vec1.x
     v1y = Obj.center.y + sfactor * Obj.vec1.y
     Obj.v1 = Point(v1x,v1y)
     Obj.vec1 = Vector(Obj.center, Obj.v1)
 
+    # New second vertex and corresponding center-to-vertex vector
+    v2x = Obj.center.x + sfactor * Obj.vec2.x
+    v2y = Obj.center.y + sfactor * Obj.vec2.y
+    Obj.v2 = Point(v2x,v2y)
+    Obj.vec2 = Vector(Obj.center, Obj.v2)
 
+    # New third vertex and corresponding center-to-vertex vector
+    v3x = Obj.center.x + sfactor * Obj.vec3.x
+    v3y = Obj.center.y + sfactor * Obj.vec3.y
+    Obj.v3 = Point(v3x,v3y)
+    Obj.vec3 = Vector(Obj.center, Obj.v3)
+
+    # New fourth vertex and corresponding center-to-vertex vector
+    v4x = Obj.center.x + sfactor * Obj.vec4.x
+    v4y = Obj.center.y + sfactor * Obj.vec4.y
+    Obj.v4 = Point(v4x,v4y)
+    Obj.vec4 = Vector(Obj.center, Obj.v4)
+
+    # Stretching a shape also change
 
 
 
