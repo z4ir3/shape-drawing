53f60a0457344f2acef18b152193b81d3090439b
Leonardo Rocchi <leonardo.rocchi@yahoo.it>
2024-05-16 22:19:49 +0200

    removing rounding

--- src/point.py
+++ src/point.py
@@ -6,7 +6,6 @@ class Point:
     """
     Point
     """
-    decround = 1
     def __init__(self, x: float, y: float):
-        self.x = round(x, self.decround)
-        self.y = round(y, self.decround)
+        self.x = x
+        self.y = y
--- src/vector.py
+++ src/vector.py
@@ -8,7 +8,6 @@ class Vector:
     """
     Vector from a first vertex called 'center' to a second 'vertex'
     """
-    decround = 1
     def __init__(self, center: Point, vertex: Point):
-        self.x = round(vertex.x - center.x, self.decround)
-        self.y = round(vertex.y - center.y, self.decround)
+        self.x = vertex.x - center.x
+        self.y = vertex.y - center.y
